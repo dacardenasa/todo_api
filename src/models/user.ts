50b8906bb639daee2b3b4acce057765bc77c483e
Diego Cardenas <diego.cardenasaleg@gmail.com>
2024-07-25 22:47:51 -0500

    added timestamps to User model to track user creation date

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,15 +1,20 @@
 import { Schema, model } from "mongoose";
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: [true, "The username of the user is required"]
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "The username of the user is required"]
+    },
+    password: {
+      type: String,
+      required: [true, "The password of the user is required"]
+    }
   },
-  password: {
-    type: String,
-    required: [true, "The password of the user is required"],
-  },
-});
+  {
+    timestamps: true
+  }
+);
 
 UserSchema.methods.toJSON = function () {
   const { __v, _id, ...user } = this.toObject();
